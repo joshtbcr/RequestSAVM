f07330b2f1701abef8d78fe0df5aaaf490d405cf
Joshua Torres <joshtbcr@gmail.com>
2020-05-09 20:27:16 -0600

    Messy code, but now works

--- content.js
+++ content.js
@@ -1,21 +1,39 @@
 //content.js
-
-
-
+//Involved HTML elements
+var spinner, requestButton, navGroup, firstGroupVmButton;  
+spinner = document.querySelector('.root-319');
 
 if(window.location.host === 'tdc.azure.net'){
+  //Show loading instead
+  document.querySelector('#root').style.opacity = "0.2"; 
+  var img = document.createElement("img");
+  var txt = document.createElement("h1");
+  txt.innerHTML = 'Loading ...';
+  var div = document.createElement("div");
+  img.src = chrome.runtime.getURL('icon.jpg');
+  div.style.top = '50%';
+  div.style.left = '50%';
+  div.style.margin = 'auto';
+  div.style.position = 'fixed';
+  div.appendChild(img);
+  div.appendChild(txt);
+  var body = document.querySelector('body');
+  body.insertBefore(div, body.firstChild);
 
-  //while($('.root-319').style.display != 'none')
+
+  
   delay(3000).then(() => {
     generalFunctionRequestVM();
   });
 }
 
-
-function generalFunctionRequestVM(){
+//Only 3 clicks needed
+//Click request VM
+//Click first group for VM
+//Click request VM from selected group
+async function generalFunctionRequestVM(){
   console.log("Running code for getting a VM");
 
-  
   let newVMPromise = new Promise((resolve, reject) => {
     //Do the required clicks 
     console.log("doClicksForVM -> Running clicks");
@@ -31,55 +49,87 @@ function generalFunctionRequestVM(){
       //Wait max 15s
       if(requestsCounter < 15){
         requestsCounter++;
+        console.log(requestsCounter + 's');
       }else{
         clearInterval(clicksInterval);
         reject(new Error("ERROR!"));
       }
       
-      console.log(requestsCounter + 's');
       
-      //Click request VM
-      let requestButton = $('button[data-automation-id="request"]');
-      let requestFirstGroupVmButton = $('button[data-automation-id="request"]');
-      let requestSelectedGroupVmButton = $('.requestVmMainOverride');
-      //$('button[title="CSS SAVM - East US 2"]');
+      requestButton = document.querySelector('button[data-automation-id="request"]');
       
+      // if(!requestButton.disabled && spinner.style.display == 'none' && clickedRequestVM === false){
       if(!requestButton.disabled && clickedRequestVM === false){
         console.log("clicksInterval -> requestButton.click()", requestButton.click());
         clickedRequestVM = true;
       } 
+
+
       
       //Click first group for VM
-      if(clickedGroupVM === false && clickedRequestVM === true){
-        console.log("clicksInterval -> requestFirstGroupVmButton.click()", requestFirstGroupVmButton.click());
-        clickedGroupVM = true;
+      if(clickedRequestVM === true && clickedGroupVM ===false){
+        navGroup = document.querySelector('div.requestVmMainOverride > div.ms-Modal-scrollableContent > div')
+        .children[1].firstElementChild.children[1].firstElementChild.firstElementChild;
+        
+        firstGroupVmButton = navGroup.firstElementChild.children[1].firstElementChild
+        .firstElementChild.firstElementChild.firstElementChild;  
+        
+        if(clickedGroupVM === false){
+          console.log("clicksInterval -> firstGroupVmButton.click()", firstGroupVmButton.click());
+          clickedGroupVM = true;
+        }
       }
       
       //Click request VM from selected group
       if(clickedRequestedGroupVM ===  false && clickedGroupVM === true){
-        console.log("clicksInterval -> requestSelectedGroupVmButton.click()", requestSelectedGroupVmButton.click());
-        clickedRequestedGroupVM = true;
-        console.log("clicksInterval -> clearclicksInterval(clicksInterval)", clearInterval(clicksInterval));
-        console.log("clicksInterval -> resolve(true)", resolve(true));
-      }
+        requestSelectedGroupVmButton = 
+        document.querySelector('div.requestVmMainOverride > div.ms-Modal-scrollableContent > div.ms-Dialog-lgHeader > div.ms-Dialog-inner > div.ms-Dialog-actions > div.ms-Dialog-actionsRight > span > button');
 
+        //Only if the button is enabled
+        // if(requestSelectedGroupVmButton.classList.value.search('is-disabled') !== -1){
+        if(true){
+          console.log("clicksInterval -> requestSelectedGroupVmButton.click()", requestSelectedGroupVmButton.click());
+          clickedRequestedGroupVM = true;
+          console.log("clicksInterval -> clearclicksInterval(clicksInterval)", clearInterval(clicksInterval));
+          console.log("clicksInterval -> resolve(true)", resolve(true));
+        }
+
+      }
     }, 1000);
     
   });
   
-  //Close tab if succeeded
-  newVMPromise.then(result =>{
-    console.log("closing tab....");
-    delay(5000).then(() => {
-      //window.close()
+
+        
+  
+  newVMPromise
+  .then(result=>{
+    //Close tab if succeeded
+    console.log("Closing tab once downloaded...");
+    delay(2000).then(() => {
+      
+      var spinner = document.querySelector('.root-319');
+      let closeInterval = setInterval(()=>{
+        if(spinner.style.display === 'none'){
+          console.log("closeInterval -> clearcloseInterval(closeInterval)", clearInterval(closeInterval));
+          window.close();
+        }
+        
+      },1000);
     });
-  }).catch(error =>{
-    
+  })
+  .catch(error =>{
     //Show error instead if unsuccesful
     alert('There was an error in downloading the RDP file: \n', error );
   });
 
+
+
+
 }
+
+
+
 
 
 
@@ -90,9 +140,12 @@ function delay(ms) {
 }
 
 
-function repeatEvery(){
+function repeatEvery(callback, ms){
 
-  let interval = setInterval(callback, 5000);
+  let interval = setInterval(()=>{
+    
+    callback;
+  }, ms);
 }
 
 function repeatEveryUntil(){
@@ -102,26 +155,9 @@ function repeatEveryUntil(){
   });
 }
 
-var spinner = $('.root-319');
-
-
-var navGroup = $('div.requestVmMainOverride > div.ms-Modal-scrollableContent > div ')
-  .children[1].firstElementChild.children[1].firstElementChild.firstElementChild;
-
-var firstGroup = navGroup.firstElementChild.children[1].firstElementChild
-  .firstElementChild.firstElementChild.firstElementChild;  
-
-var requestSelectedGroupVmButton = $('#id__57');
 
 
 
-
-
-//Only 3 clicks needed
-//Click request VM
-
-//Click first group for VM
-//Click request VM from selected group
 function doClicksForVM(){
   console.log("doClicksForVM -> Running clicks");
 
@@ -145,7 +181,7 @@ function doClicksForVM(){
     
     //Click request VM
     let requestButton = $('button[data-automation-id="request"]');
-    let requestFirstGroupVmButton = $('button[data-automation-id="request"]');
+    let firstGroupVmButton = $('button[data-automation-id="request"]');
     let requestSelectedGroupVmButton = $('.requestVmMainOverride');
     //$('button[title="CSS SAVM - East US 2"]');
     //$('button[title="CSS SAVM - East US 2"]');
